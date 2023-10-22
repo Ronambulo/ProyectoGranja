6bc84dfec4af37f95f3a727dc1dcfcf2e1d5dd49
Ronambulo <enriquerodridel@gmail.com>
2023-10-22 17:00:59 +0200

    Movimiento + Tiles Usables

--- ProyectoGranja/Assets/Scripts/PlayerController.cs
+++ ProyectoGranja/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float speed;
+    
+    // Update is called once per frame
+    void FixedUpdate()
     {
+        Animator animator = GetComponent<Animator>();
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Animaciones(horizontal, vertical, animator);
         
+        Vector3 direction = new Vector3(horizontal, vertical);
+
+        transform.Translate(direction * speed);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Animaciones(float horizontal, float vertical, Animator animator)
     {
-        
+        if (horizontal != 0 || vertical != 0)
+        {
+            animator.SetBool("Walking", true);
+            if(horizontal < 0)
+            {
+                transform.localScale = new Vector3(-1 , 1, 0);
+            }
+            else if(horizontal > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 0);
+            }
+        }else
+        {
+            animator.SetBool("Walking", false);
+        }
     }
+
 }
