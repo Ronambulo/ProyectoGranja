9be96fa8d9d24375c58a9f934e12cd13adf6be80
sasahershko <sarayhershko@gmail.com>
2023-11-02 12:38:28 +0100

    comentarios

--- ProyectoGranja/Assets/Scripts/SequenceAnimator.cs
+++ ProyectoGranja/Assets/Scripts/SequenceAnimator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SequenceAnimator : MonoBehaviour
 {
+    //Creo lista
     List<Animator> _animators;
     public float tiempoIntermedio = 0.15f;
     public float tiempoAcabado = 0.5f;
@@ -11,19 +12,26 @@ public class SequenceAnimator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Inicializamos la lista con todos los componentes Animator
         _animators = new List<Animator>(GetComponentsInChildren<Animator>());
+
+        //Inicializa la secuencia de la animación
         StartCoroutine(DoAnimation());
     }
 
-    // PARA LOOPEAR PARA SIEMPRE
+    // PARA LOOPEAR PARA SIEMPRE - IEnumerator, para realizar operaciones asincrónicas ( no simultáneo, no coincidente)
     IEnumerator DoAnimation()
     {
         while (true)
         {
+            //Itera a través de todos los componentes Animator en la lista _animators
             foreach (var animator in _animators)
             {
+                //activa el triger DoAnimation, dentro de Animator
                 animator.SetTrigger("DoAnimation");
-                yield return new WaitForSeconds(tiempoIntermedio); // "WaitForseconds" estaba mal escrito, debe ser "WaitForSeconds"
+
+                //Hace que el programa espere durante un período de tiempo especificado
+                yield return new WaitForSeconds(tiempoIntermedio); 
             }
                 //creamos un delay para que cuando llegue al final, se espere un poco hasta volver a empezar
                 yield return new WaitForSeconds(tiempoAcabado);
