69959dad5808395e7efe50c183492ab3060da429
sasahershko <sarayhershko@gmail.com>
2023-11-02 18:35:04 +0100

    Revert "a"
    This reverts commit d1595986ea0a2a1005463fe555084473222c83f4, reversing
    changes made to 70212a11734cd2c4f1550c6006357bce6089d2ca.

--- ProyectoGranja/Assets/Scripts/Inventory/OnHoverScript.cs
+++ ProyectoGranja/Assets/Scripts/Inventory/OnHoverScript.cs
@@ -25,4 +25,11 @@ public class OnHoverScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         animator.SetBool("OnHoverBool", false);
         enter = false;
     }
+
+    private void OnEnable()
+    {
+        animator.SetBool("OnHoverBool", false);
+        enter = false;
+    }
+
 }
