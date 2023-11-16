6a15ac11790bef422cc0800d0a2b3d726d38e772
Adrigg2004 <adrianggallego.2004@gmail.com>
2023-11-16 18:15:47 +0100

    TileHover principio
    Añadidos las variables para hacer un tileHover, ademas he modificado las condiciones para que sea visible el cursor (todavia falta añadir una manera de ver el objeto en mano)

--- ProyectoGranja/Assets/Scripts/CursorManager.cs
+++ ProyectoGranja/Assets/Scripts/CursorManager.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CursorManager : MonoBehaviour
 {
-
+    [SerializeField] public Player player;
     [SerializeField] public Texture2D cursorDefault;
     [SerializeField] public Texture2D cursorAtaque;
     [SerializeField] public GameObject inventoryPanel;
-    [SerializeField] public GameObject hotbar;
+    [SerializeField] public GameObject pauseMenu;
+    [SerializeField] public Tilemap interactableMap;
+    [SerializeField] public Tilemap floor;
+    private Grid grid;
 
     private Vector2 cursorHotspot;
 
+    private Vector2 cursorPosition;
+    private Vector2 previousCursorPosition = new Vector2();
+
     void Start()
     {
+<<<<<<< Updated upstream
         cursorHotspot = new Vector2(cursorDefault.width / 2, cursorDefault.height / 2);
+=======
+        grid = gameObject.GetComponent<Grid>();
+        cursorHotspot = new Vector2(cursorDefault.width/ divisor, cursorDefault.height/ divisor);
+>>>>>>> Stashed changes
         Cursor.SetCursor(cursorDefault,cursorHotspot, CursorMode.Auto);
     }
 
@@ -22,10 +34,16 @@ public class CursorManager : MonoBehaviour
     void Update()
     {
 
+<<<<<<< Updated upstream
         if (&& inventoryPanel.activeSelf == false)
         {
             Cursor.visible = true;
             cursorHotspot = new Vector2(cursorDefault.width / 2, cursorDefault.height / 2);
+=======
+        if ((inventoryPanel.activeSelf == false || pauseMenu.activeSelf == false))
+        {
+            Cursor.visible = true;
+>>>>>>> Stashed changes
             Cursor.SetCursor(cursorAtaque, cursorHotspot, CursorMode.Auto);
         }
         else
@@ -33,8 +51,12 @@ public class CursorManager : MonoBehaviour
             if (inventoryPanel.activeSelf==true)
             {
                 Cursor.visible = true;
+<<<<<<< Updated upstream
                 cursorHotspot = new Vector2(cursorDefault.width / 2, cursorDefault.height / 2);
                 Cursor.SetCursor(cursorDefault cursorHotspot, CursorMode.Auto);
+=======
+                Cursor.SetCursor(cursorDefault, cursorHotspot, CursorMode.Auto);
+>>>>>>> Stashed changes
             }
             else
             {
@@ -42,5 +64,10 @@ public class CursorManager : MonoBehaviour
             }
 
         }
+    }
+
+    void tileHover()
+    {
+
     }
 }
