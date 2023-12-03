77b513f33626f387f6b068b2d706bc9f0d064519
Ronambulo <enriquerodridel@gmail.com>
2023-12-03 11:51:42 +0100

    Cambios Adrian

--- ProyectoGranja/Assets/Scripts/Managers/CursorManager.cs
+++ ProyectoGranja/Assets/Scripts/Managers/CursorManager.cs
@@ -1,29 +1,39 @@
+using System;
+//using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.SceneManagement;
 
 public class CursorManager : MonoBehaviour
 {
-    [SerializeField] public Player player;
+    [SerializeField] public GameObject player;
+    [SerializeField] public ToolBar_UI toolBar_UI;
     [SerializeField] public Texture2D cursorDefault;
     [SerializeField] public Texture2D cursorAtaque;
     [SerializeField] public GameObject inventoryPanel;
     [SerializeField] public GameObject pauseMenu;
-    [SerializeField] public Tilemap interactableMap;
-    [SerializeField] public Tilemap floor;
-    private Grid grid;
+    [SerializeField] public Tile []listaCultivo;
+
+    private Tilemap crops;
+    private Tilemap floor;
+
+    private List<string> listaEspadas = new List<string> { "Diamond Sword", "Gold Sword", "Bronze Sword" , "Iron Sword" };
+    private string objetoEnMano;
+    private enum colores { noColor, red, green, gray }
+    private List<Color> listaColores = new List<Color>{Color.white, Color.red, Color.green, new Color(0.75f, 0.75f, 0.75f, 1f) }; 
+
 
     private int divisor = 10;
     private Vector2 cursorHotspot;
+    private Vector3Int previousCursorPosition = new Vector3Int();
 
-    private Vector2 cursorPosition;
-    //private Vector2 previousCursorPosition = new Vector2();
+    private Scene prevScene= new Scene();
+    private bool gameStarted=false;
 
     void Start()
     {
-
-        grid = gameObject.GetComponent<Grid>();
         cursorHotspot = new Vector2(cursorDefault.width/ divisor, cursorDefault.height/ divisor);
         Cursor.SetCursor(cursorDefault,cursorHotspot, CursorMode.Auto);
     }
@@ -31,8 +41,37 @@ public class CursorManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Grid grid = FindObjectOfType<Grid>();
+        Vector3Int playerPosition = new Vector3Int(
+            Mathf.RoundToInt(player.transform.position.x),
+            Mathf.RoundToInt(player.transform.position.y),
+            Mathf.RoundToInt(player.transform.position.z)
+            );
+        Scene currentScene = SceneManager.GetActiveScene();
+        objetoEnMano = toolBar_UI.nombreSeleccionado;
+        Vector3Int cursorPosition = grid.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        bool boolReachable = Math.Abs(cursorPosition.x - playerPosition.x * 2) <= 3 && Math.Abs(cursorPosition.y - playerPosition.y * 2) <= 3;
 
-        if (inventoryPanel.activeSelf == false)
+        if (currentScene.name=="EscenaGranja"&&!gameStarted)
+            gameStarted=true;
+        if (!currentScene.Equals(prevScene))
+        {
+            prevScene = currentScene;
+            Tilemap[] tilemaps = FindObjectsOfType<Tilemap>();
+            foreach (Tilemap tilemap in tilemaps)
+            {
+                if (tilemap.name == "Crops")
+                {
+                    crops = tilemap;
+                }
+                if (tilemap.name == "Floor")
+                {
+                    floor = tilemap;
+                }
+            }
+        }
+        
+        if (inventoryPanel.activeSelf == false && listaEspadas.Contains(objetoEnMano))
         {
             Cursor.visible = true;
             cursorHotspot = new Vector2(cursorDefault.width / divisor, cursorDefault.height / divisor);
@@ -40,7 +79,7 @@ public class CursorManager : MonoBehaviour
         }
         else
         {
-            if (inventoryPanel.activeSelf==true)
+            if (inventoryPanel.activeSelf == true || pauseMenu.activeSelf == true)
             {
                 Cursor.visible = true;
 
@@ -49,14 +88,39 @@ public class CursorManager : MonoBehaviour
             }
             else
             {
+                if (gameStarted == true && grid != null)
+                {
+                    if (!cursorPosition.Equals(previousCursorPosition))
+                    {
+                        floor.SetTileFlags(previousCursorPosition, TileFlags.None);
+                        floor.SetColor(previousCursorPosition, listaColores[(int)colores.noColor]);
+                        if (objetoEnMano=="Hoe" && currentScene.name == "EscenaGranja")
+                        {
+                            if (boolReachable)
+                            {
+                                floor.SetTileFlags(cursorPosition, TileFlags.None);
+                                floor.SetColor(cursorPosition, listaColores[(int)colores.green]);
+                            }
+                            else
+                            {
+                                floor.SetTileFlags(cursorPosition, TileFlags.None);
+                                floor.SetColor(cursorPosition, listaColores[(int)colores.red]);
+                            }
+                        }
+                        else
+                        {
+                            floor.SetTileFlags(cursorPosition, TileFlags.None);
+                            floor.SetColor(cursorPosition, listaColores[(int)colores.gray]);
+                        }
+                    }
+                }
                 Cursor.visible = false;
             }
-
+            if (objetoEnMano == "Hoe" && Input.GetMouseButtonDown(0) && crops.GetTile(cursorPosition) != null && boolReachable)
+            {
+                floor.SetTile(cursorPosition, listaCultivo[new System.Random().Next(0, 3)]);
+            }
+            previousCursorPosition = cursorPosition;
         }
-    }
-
-    void tileHover()
-    {
-
     }
 }
