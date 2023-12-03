55e6ac3fd560abfdd48b09c629b060eb03df4003
sasahershko <sarayhershko@gmail.com>
2023-12-03 01:09:24 +0100

    voy a morir

--- ProyectoGranja/Assets/Scripts/Music.cs
+++ ProyectoGranja/Assets/Scripts/Music.cs
@@ -1,21 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Music : MonoBehaviour
 {
     [Header("----------- Audio Source -----------")]
-    [SerializeField] AudioSource musicSource;
+    public AudioSource musicTitle;
+    public AudioSource musicFarm;
+    public AudioSource musicDeath;
+    public AudioSource musicMazmorra;
 
-    [Header("----------- Audio Clip -----------")]
+    public string ThisScene;
+
+    /*[Header("----------- Audio Clip -----------")]
     public AudioClip background;
-
+    public AudioClip title;
+    public AudioClip death;
+    public AudioClip mazmorra;*/
 
     // Start is called before the first frame update
     void Start()
     {
-        musicSource.clip = background;
-        musicSource.Play();
+        musicTitle.Stop();
+        musicFarm.Stop();
+        musicDeath.Stop();
+        musicMazmorra.Stop();
     }
 
     // Update is called once per frame
