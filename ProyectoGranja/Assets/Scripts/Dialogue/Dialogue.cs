57f24ed572e8add632239369a67b927110611c96
s-ol-jun <148479598+s-ol-jun@users.noreply.github.com>
2023-12-03 18:04:22 +0100

    Dialogo DEFINITIVO FUNCIONA
    Recordatorio: asegurarse de que el componente script del npc está puesto correctamente al hacer merge

--- ProyectoGranja/Assets/Scripts/Dialogue/Dialogue.cs
+++ ProyectoGranja/Assets/Scripts/Dialogue/Dialogue.cs
@@ -6,6 +6,7 @@ using TMPro;
 
 public class Dialogue : MonoBehaviour
 {
+<<<<<<< Updated upstream
     [SerializeField] private TextMeshProUGUI textoDialogo;
     [SerializeField] private int numFrases;
     [SerializeField] private string[] frasesDialogo;
@@ -13,19 +14,35 @@ public class Dialogue : MonoBehaviour
     [SerializeField] private GameObject DialoWindow;
     [SerializeField] private GameObject ToolBar;
     private int index;
+=======
+    public GameObject dialoguePanel;
+    
+    public TextMeshProUGUI dialogueText;
+    public string[] dialogue;
+    private int index = 0;
+>>>>>>> Stashed changes
 
-    void Awake()
+    public float wordSpeed;
+    public bool playerIsClose;
+
+
+    void Start()
     {
+<<<<<<< Updated upstream
         textoDialogo.text = string.Empty;
         DialoWindow = GameObject.FindWithTag("VentanaDialogo");
         ToolBar = GameObject.FindWithTag("ToolBar");
         DialoWindow.GetComponent<Image>().enabled = false;
+=======
+        dialogueText.text = "";
+        RemoveText();
+>>>>>>> Stashed changes
     }
 
-
-   // Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         if(DialoWindow == null){
             DialoWindow = GameObject.FindWithTag("VentanaDialogo");
             ToolBar = GameObject.FindWithTag("ToolBar");
@@ -47,42 +64,89 @@ public class Dialogue : MonoBehaviour
                 ToolBar.SetActive(true);
                 DialoWindow.GetComponent<Image>().enabled = false;
                  DialoWindow.SetActive(false);
+=======
+        if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
+        {
+            if (!dialoguePanel.activeInHierarchy)
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+>>>>>>> Stashed changes
             }
+            else if (dialogueText.text == dialogue[index])
+            {
+                NextLine();
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.Q) && dialoguePanel.activeInHierarchy)
+        {
+            RemoveText();
         }
     }
+<<<<<<< Updated upstream
        
     void StartDialogue() {
         index = 0;
         StartCoroutine(TypeLine());
+=======
+
+    public void RemoveText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+>>>>>>> Stashed changes
     }
 
-    IEnumerator TypeLine() {
-        textoDialogo.text = frasesDialogo[index];
-        return null;
+    IEnumerator Typing()
+    {
+        foreach(char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
     }
 
-    void NextLine() {
-        if (index < numFrases-1) {
+    public void NextLine()
+    {
+        if (index < dialogue.Length - 1)
+        {
             index++;
+<<<<<<< Updated upstream
             textoDialogo.text = string.Empty;
             StartCoroutine(TypeLine());
         } else {
             gameObject.SetActive(false);
+=======
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            RemoveText();
+>>>>>>> Stashed changes
         }
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            playerInDialo = true;
+            playerIsClose = true;
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other) {
+    private void OnTriggerExit2D(Collider2D other)
+    {
         if (other.CompareTag("Player"))
         {
+<<<<<<< Updated upstream
             playerInDialo = false;
+=======
+            playerIsClose = false;
+            RemoveText();
+>>>>>>> Stashed changes
         }
     }
 }
